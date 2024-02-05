1b003eb9985511c3c34bf692e9beb073a53ce2f1
koudaiii <cs006061@gmail.com>
2024-02-05 21:47:33 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/elazarl/goproxy"
+)
+
+func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	proxy.Logger = log.New(log.Writer(), log.Prefix(), log.Flags())
+	// If you have any other proxy server, you can use only this proxy using this option.
+	// ex. Client(curl --proxy localhost:8080 https://koudaiii.com) -> this proxy:8080 -> koudaiii.com:443
+	proxy.ConnectDial = nil
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+}
