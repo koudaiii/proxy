ae64e95f186124f4fed66ddef5214a27c4aab997
koudaiii <cs006061@gmail.com>
2024-02-06 01:52:50 +0900

    add log

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 	proxy.Logger = log.New(log.Writer(), log.Prefix(), log.Flags())
+	log.Println("Proxy server started at localhost:8080")
 	// If you have any other proxy server, you can use only this proxy using this option.
 	// ex. Client(curl --proxy localhost:8080 https://koudaiii.com) -> this proxy:8080 -> koudaiii.com:443
 	proxy.ConnectDial = nil
